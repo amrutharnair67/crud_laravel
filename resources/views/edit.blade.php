38d13de5ccaeb910020db4b16658dfd3db92a63e
Amrutha R Nair <rnairamrutha67@gmail.com>
2023-09-30 06:28:17 +0530

    first commit

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <form method="post" action="{{ route('update-student') }}">
+        @csrf
+        <input type="hidden" name="id" value="{{ $data->id }}">
+        Name <input type="text" name="name" value="{{ $data->name }}"><br>
+        <button type="submit">Submit</button>
+    </form>
+    </center>
+</body>
+</html>
